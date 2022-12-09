5cf53f995c3ba198048f148a4108522bdcdf7f15
Eugene Huang <eugene@goforward.com>
2022-12-09 02:24:57 -0800

    clean up rope moves

--- src/advent_2022/day_9.clj
+++ src/advent_2022/day_9.clj
@@ -4,31 +4,35 @@
 (defn read-input [file-name]
   (map str/trim-newline (str/split (slurp file-name) #"\n")))
 
-(defn expanded-input [input]
+(defn expanded-input
+  "Expand input sequence turning instructions like 'R 2, U 1' into 'R R U'."
+  [input]
   (flatten
    (map (fn [row]
           (let [parts (rest (re-find #"(\S+) (\d+)" row))]
             (repeat (Integer/parseInt (second parts)) (first parts)))) input)))
 
-(defn move [steps n]
+(defn move
+  "Move a rope with n knots through the steps. Returns final knot locs and a set of all locs the tail has seen."
+  [steps n-knots]
   (letfn [(move-head [pos step]
             (map + pos (case step "R" [0 1] "L" [0 -1] "D" [1 0] "U" [-1 0])))
           (move-knot [parents knot]
             (let [parent (last parents)
                   delta (map - parent knot)
                   new-knot (cond
-                             (every? #(= 2 %) (map abs delta)) (mapv + (map #(/ % 2) delta) knot) ;; diagonal move
-                             (= 2 (first delta)) [(inc (first knot)) (second parent)]             ;; weird snake moves
-                             (= -2 (first delta)) [(dec (first knot)) (second parent)]
-                             (= 2 (second delta)) [(first parent) (inc (second knot))]
-                             (= -2 (second delta)) [(first parent) (dec (second knot))]
+                             (every? #(= 2 %) (map abs delta)) (mapv + (map #(quot % 2) delta) knot) ;; diagonal move
+                             (some #(= 2 %) (map abs delta)) (let [scootch (mapv #(quot % 2) delta)] ;; weird snake moves
+                                                               (if (= 1 (abs (first scootch)))
+                                                                 [(+ (first knot) (first scootch)) (second parent)]
+                                                                 [(first parent) (+ (second knot) (second scootch))]))
                              :else knot)]
               (conj parents new-knot)))
           (move-knots [acc step]
             (let [new-head (move-head (first (:knots acc)) step)
                   new-knots (reduce move-knot [new-head] (rest (:knots acc)))]
               {:knots new-knots :tail-seen (conj (:tail-seen acc) (last new-knots))}))]
-    (reduce move-knots {:knots (repeat n [0 0]) :tail-seen #{}} steps)))
+    (reduce move-knots {:knots (repeat n-knots [0 0]) :tail-seen #{}} steps)))
 
 (defn -main [& args]
   (when (not= (count args) 1)
