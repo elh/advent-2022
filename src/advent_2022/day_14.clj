f217702c37723a680b272c64ec09117e826739bc
Eugene Huang <eugene@goforward.com>
2022-12-15 01:21:55 -0800

    add day 14 p2 solution

--- src/advent_2022/day_14.clj
+++ src/advent_2022/day_14.clj
@@ -36,10 +36,26 @@
           (not (contains? occupancy (map + sand-loc [1 1]))) (recur occupancy (map + sand-loc [1 1]))
           :else (recur (conj occupancy sand-loc) [500 0]))))))
 
+(defn pour-sand-p2 [occupancy]
+  (let [lowest (last (sort (map #(second %) occupancy)))
+        floor (+ lowest 2)]
+    (letfn [(open? [o l]
+              (if (>= (second l) floor)
+                false
+                (not (contains? o l))))]
+      (loop [occupancy occupancy sand-loc [500 0]]
+        (cond
+          (open? occupancy (map + sand-loc [0 1])) (recur occupancy (map + sand-loc [0 1]))
+          (open? occupancy (map + sand-loc [-1 1])) (recur occupancy (map + sand-loc [-1 1]))
+          (open? occupancy (map + sand-loc [1 1])) (recur occupancy (map + sand-loc [1 1]))
+          :else (if (= sand-loc [500 0])
+                  (conj occupancy sand-loc)
+                  (recur (conj occupancy sand-loc) [500 0])))))))
+
 (defn -main [& args]
   (when (not= (count args) 1)
     (throw (Exception. (format "FAIL: expects input file as cmdline arg. got %d args" (count args)))))
   (let [input (read-input (first args))
         occupancy (make-occupancy input)]
-    (println "part 1:" (- (count (pour-sand occupancy)) (count occupancy)))
-    (println "part 2:" "TODO")))
+    (println "part 1:" (time (- (count (pour-sand occupancy)) (count occupancy))))
+    (println "part 2:" (time (- (count (pour-sand-p2 occupancy)) (count occupancy))))))
